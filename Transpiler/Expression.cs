dc8adde9d94140c6eec9b8c20a06f8958444179f
k-srivastava <srivastavak1223@gmail.com>
2024-10-17 01:58:08 +0530

    Created expression and identifier nodes.

---
+++ Transpiler/Expression.cs
@@ -0,0 +1,22 @@
+namespace Transpiler;
+
+/// <summary>
+/// General expression node supported by DBIR.
+/// </summary>
+/// <param name="Token">Token corresponding to the expression.</param>
+public abstract record Expression(Token Token)
+{
+    public abstract override string ToString();
+}
+
+/// <summary>
+/// General identifier node supported by DBIR.
+/// </summary>
+/// <param name="Token">Token corresponding to the identifier.</param>
+public record Identifier(Token Token) : Expression(Token)
+{
+    public override string ToString()
+    {
+        return Token.Lexeme;
+    }
+}
